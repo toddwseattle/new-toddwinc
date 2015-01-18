41152e31b56deaa9b4e101da69e89ebd5fabc6ed
Todd Warren <todd@warrenfamily.org>
2015-01-18 11:14:08 -0800

    consolidated config functon $mdThemingProvider not behaving properly yet but commented out in app.js

--- app/app.js
+++ app/app.js
@@ -2,11 +2,15 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    'ngMaterial',
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+])
+.config(['$routeProvider','$mdThemingProvider', function ($routeProvider,$mdThemingProvider) {
+        $routeProvider.otherwise({ redirectTo: '/view1' });
+//        $mdThemingProvider.theme('default').primaryColor('grey');
+
+    }])
+;
